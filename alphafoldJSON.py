91abce128f087ec9fa9f1bb32247fc6da3dcc634
FloAvis <152602773+FloAvis@users.noreply.github.com>
2024-09-19 12:34:33 +0200

    Commenting alphafoldJSON.py

--- alphafoldJSON.py
+++ alphafoldJSON.py
@@ -1,21 +1,32 @@
+"""
+This script creates a JSON file for the AlphaFold3 webserver for batch upload.
+Input: FOldername of fasta files which you want to upload
+Output: JSOn file in the right formatting for upload
+"""
+
 import json
 import os
 from Bio import SeqIO
 
+
 def createJSON(output_json, folder_path):
+
+    #List for collecting the entries
     listOfEntries = []
 
-
+    #Going through all files
     for file_name in os.listdir(folder_path):
         if file_name.endswith(".fasta"):
             file_path = os.path.join(folder_path, file_name)
             print(f"Processing {file_name}...")
 
-            record = SeqIO.read(file_path, "fasta")
+            #It is assumed that only one sequence is in each file
+            record = SeqIO.read(file_path, "fasta") 
 
             name = str(record.id)[0:4]
             sequence = str(record.seq)
 
+            #structure of the JSOn file.
             dictionary = {
                 "name": name,
                 "modelSeeds": [],
@@ -31,7 +42,6 @@ def createJSON(output_json, folder_path):
 
             listOfEntries.append(dictionary)
 
-            #print(dictionary)
 
 
 
